74772c77f8ee3be23b2e209d6469a6e765afac1e
vybzzhack <108887910+vybzzhack@users.noreply.github.com>
2024-08-04 19:38:14 +0300

    Add files via upload

---
+++ weatherdata.sql
@@ -0,0 +1,15 @@
+SELECT COUNT(*)
+FROM weathertb
+WHERE WindSpeed > 24 AND Visibility = 25;
+
+SELECT AVG(Visibility) AS MeanVisibility
+FROM weathertb;
+
+SELECT *
+FROM weathertb
+WHERE Weather = 'Clear';
+
+SELECT COUNT(*)
+FROM weathertb
+WHERE Wind Speed_km/h= 4;
+
